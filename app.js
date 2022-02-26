cf0d43e07f522fcb4f639831017a71e916cf8b04
Daniel Lopez <dlopezre95@gmail.com>
2022-02-26 12:59:24 +0100

    env and cors

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 require("dotenv/config");
 require("./db");
 const express = require("express");
+const cors = require('cors')
 
 const { isAuthenticated } = require("./middleware/jwt.middleware");
 const allRoutes = require("./routes");
 const authRouter = require("./routes/auth.routes");
 const protectedRoute = require("./routes/protected.routes");
+const { application } = require("express");
 
 const app = express();
 
 require("./config")(app);
+
+app.use(cors({ origin: process.env.ORIGIN || 'http://localhost:3000' }));
 
 app.use("/api", allRoutes);
 app.use("/api/protected", isAuthenticated, protectedRoute);

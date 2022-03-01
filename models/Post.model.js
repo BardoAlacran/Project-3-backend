e9718661e8a94258fec64d6cd4bb19f3f3c6df91
Daniel Lopez <dlopezre95@gmail.com>
2022-03-01 17:44:14 +0100

    post model updated

--- models/Post.model.js
+++ models/Post.model.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const postSchema = new Schema({
-  user: { type: string, unique: true, required: true },
-  date: { type: date },
+  user: { type: String, unique: true, required: true },
+  date: { type: Date },
   isFav: { type: Boolean, default: false },
-  body: { type: string, required: true },
-  level: { type: string, enum: ['Easy', 'Medium', 'Hard', 'Godlike'] },
-  theme: { type: string, enum: ['Gardening', 'Computing', 'Gaming', 'Science'] },
+  body: { type: String, required: true },
+  level: { type: String, enum: ['Easy', 'Medium', 'Hard', 'Godlike'] },
+  theme: { type: String, enum: ['Gardening', 'Computing', 'Gaming', 'Science'] },
 });
 
 module.exports = model('Post', userSchema);

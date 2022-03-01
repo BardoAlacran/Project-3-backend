6da8a7fbf770bee9c4b5b1f9458b091bb6888d1f
Daniel Lopez <dlopezre95@gmail.com>
2022-03-01 21:08:45 +0100

    postRoutes done

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -35,12 +35,37 @@ router.post('/add', (req, res, next) => {
     })
     .catch(error => {
       console.log(error);
-      res.status(401).json({ message: 'something went wrong' });
+      res.status(401).json({ message: 'add went wrong' });
     });
 });
 
-router.put('/post/:id', (req, res, next) => {});
+router.put('/post/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { body, level, theme } = req.body;
 
-router.delete('/post/:id', (req, res, next) => {});
+  Post.findByIdAndUpdate(id, { body, level, theme })
+    .then(updatedPost => {
+      console.log(updatedPost);
+      res.json(updatedPost);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(401).json({ message: 'the edit went wrong' });
+    });
+});
+
+router.delete('/post/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  Post.findByIdAndDelete(id)
+    .then(() => {
+      res.json({ message: 'delete completed' });
+      //hay que redireccionar a algún sitio, quizás desde el front
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(401).json({ message: 'the delete went wrong' });
+    });
+});
 
 module.exports = router;
